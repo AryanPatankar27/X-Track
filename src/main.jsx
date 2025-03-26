a9ddcbc63294aeb603c7fa8cb565eb655763c5ad
Aryan Patankar <aryanpatankar27@gmail.com>
2025-03-26 12:04:28 +0530

    change in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
+const container = document.getElementById('root');
+if (!container) {
+  throw new Error("Failed to find the root element. Please ensure your index.html contains a <div id='root'></div>.");
+}
+
+const root = createRoot(container);
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
